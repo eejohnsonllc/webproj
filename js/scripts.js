4b456554ba571d68903d82dceb3851245fceac98
ReichYang <yukunyang9508@gmail.com>
2020-07-29 22:34:42 -0400

    bio2

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,98 @@
+import { projects } from '/projects.js'
 $(document).ready(function () {
 
+
+
+
+
+  $('#slideshow').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
     // adding grid of cards
-    $grid = $("#grid")
-    let numOfRows = 2
-    let numOfCols = 3
-    for (let i = 0; i < numOfRows; i++) {
-        // let $row = $(`<div class="columns is-variable is-3">`)
-        for (let j = 0; j < numOfCols; j++) {
-            let $card = $(`<div class="column is-one-third">
-        <div class="card-image">
-          <figure class="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-          </figure>
-        </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">John Smith</p>
-              <p class="subtitle is-6">@johnsmith</p>
-            </div>
+    let $grid = $("#grid")
+
+    projects.map(function(pro) {
+
+      let $card = $(`<div class="column is-one-third-desktop is-full-mobile aos-init aos-animate">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <a href=${pro.link} target="_blank"><img src="${pro.projectimg}" alt=""></a>
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">${pro.projectname}</p>
           </div>
+        </div>
+    
+        <div class="content">
+        ${pro.projctdescription} 
+          
+        </div>
+
+        </div>
+        <div class="tags"></div>
+        </div>
+      </div>
+    </div>`)
+
+    let $tags = $card.find('.tags')
+        pro.projectkeywords.map(function(word) {
+            let $tag = $('<span class="tag"></span>').html(word)
+            $tags.append($tag)
+        })
+
+  
+  
+  
+    $grid.append($card)
+  })
+
+
+    // let numOfRows = 2
+    // let numOfCols = 3
+    // for (let i = 0; i < numOfRows; i++) {
+    //     // let $row = $(`<div class="columns is-variable is-3">`)
+    //     for (let j = 0; j < numOfCols; j++) {
+    //         let $card = $(`<div class="column is-one-third-desktop is-full-mobile aos-init aos-animate">
+    //     <div class="card-image">
+    //       <figure class="image is-4by3">
+    //         <img src="./pictures/pro1.gif" alt="Placeholder image">
+    //       </figure>
+    //     </div>
+    //     <div class="card-content">
+    //       <div class="media">
+    //         <div class="media-content">
+    //           <p class="title is-4">John Smith</p>
+    //           <p class="subtitle is-6">@johnsmith</p>
+    //         </div>
+    //       </div>
       
-          <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            <a href="#">#css</a> <a href="#">#responsive</a>
-            <br>
-            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-          </div>
-        </div>
-      </div>`)
+    //       <div class="content">
+    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    //         Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+    //         <a href="#">#css</a> <a href="#">#responsive</a>
+    //         <br>
+    //         <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    //       </div>
+    //     </div>
+    //   </div>`)
 
-            $grid.append($card)
-
-
-        }
-        // $grid.append($row)
+    //         $grid.append($card)
 
 
+    //     }
+    //     // $grid.append($row)
 
 
-    }
+
+
+    // }
 
 
 
---
+++ projects.js
@@ -0,0 +1,27 @@
+export let projects = [
+    {
+    projectid: 1,
+    projectname: 'Cumulative State Policy Counts over Time',
+    projectkeywords: ['State Policy', 'Tableau','Map'],
+    projctdescription: 'Cumulative State Policy Counts over Time 2',
+    projectspan: [],
+    projectimg: './pictures/proj1.gif',
+    link:'https://public.tableau.com/profile/evan.johnson#!/vizhome/CumulativeStatePolicyCountsoverTime2/Dashboard1'
+},
+{
+    projectid: 2,
+    projectname: 'State Portfolio Spread',
+    projectkeywords: ['Policy', 'Bar Chart', 'Tableau'],
+    projctdescription: 'State Portfolio Spread',
+    projectspan: [],
+    projectimg: './pictures/proj2.gif',
+    link:'https://amd-visualization-armanios-et-al-2020.now.sh/'
+},
+{
+    projectid: 3,
+    projectname: 'Research C',
+    projectkeywords: ['Social Media', 'Library Service', 'Information Behavior'],
+    projctdescription: 'DescriptionW ',
+    projectspan: [],
+    projectimg: './pictures/proj2.gif'
+}  ]
