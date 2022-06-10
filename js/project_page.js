07ea83226187c6dfe32fa2be9d7f20804a10adae
ReichYang <yukunyang9508@gmail.com>
2022-06-09 22:31:09 -0400

    d

---
+++ js/project_page.js
@@ -0,0 +1,91 @@
+import {
+  projects
+} from '/projects_project_page.js'
+$(document).ready(function () {
+
+  var removeActive = () => {
+    $('li').each(function () {
+      $(this).removeClass('is-active');
+    });
+  }
+
+  $('li').on('click', (e) => {
+    var tabName = (e.currentTarget.id);
+    removeActive();
+    console.log(tabName)
+    $('#' + tabName).addClass('is-active');
+    $('#' + tabName + '-content').removeClass('hidden');
+    check_active()
+  });
+
+
+  let check_active = function () {
+    let $selected_tab = $('li.is-active')[0].id
+    console.log($selected_tab)
+
+    if ($selected_tab == 'all-tab') {
+      $columns.empty()
+      projects.map(function (pro) {
+        let $temp_card = $(`<div class="column is-4">
+                <div class="card is-shady">
+                  <div class="card-image">
+                    <figure class="image is-4by3">
+                      <img src="${pro.projectimg}" alt="Placeholder image" class="modal-button" data-target="modal-image2">
+                    </figure>
+                  </div>
+                  <div class="card-content">
+                    <div class="content">
+                      <h4>${pro.projectname}</h4>
+                      <p>${pro.projctdescription}</p>
+                      <button class="button is-link is-light"><a href="${pro.link}">See More</a></button>
+                    </div>
+                  </div>
+                </div>
+              </div>`)
+
+
+            $temp_card.appendTo($columns).fadeIn(300)
+      })
+    } else {
+
+      let $filtered_cards = projects.filter(proj => proj.tab.includes($selected_tab));
+
+      console.log($filtered_cards)
+      // $columns.fadeOut()
+      $columns.empty()
+      // $columns.findAll('.column').map(column => column.fadeOut())
+      $filtered_cards.map(function (pro) {
+        let $temp_card = $(`<div class="column is-4">
+            <div class="card is-shady">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="${pro.projectimg}" alt="Placeholder image" class="modal-button" data-target="modal-image2">
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <h4>${pro.projectname}</h4>
+                  <p>${pro.projctdescription}</p>
+                  <button class="button is-link is-light"><a href="${pro.link}">See More</a></button>
+                </div>
+              </div>
+            </div>
+          </div>`)
+
+
+        $temp_card.appendTo($columns).fadeIn(300)
+        // $columns.fadeIn()
+      })
+    }
+  }
+
+  let $columns = $('#cards-columns')
+
+
+  check_active()
+
+
+
+
+
+})
---
+++ projects_project_page.js
@@ -0,0 +1,32 @@
+export let projects = [
+    {
+    projectid: 1,
+    projectname: 'Carolina Across 100 Initiative Survey Data Analysis',
+    projectkeywords: ['State Policy', 'Tableau','Map','Yearly Evolution'],
+    projctdescription: "We were hired by the Carolina Across 100 Initiative out of the UNC School of Government to analyze survey data that asked respondents from all across the state about the effect of COVID-19 on their organizations and communities. Using a suite of data analytics tools, we were able to identify common areas of concern and trends in expectations about recovery from the pandemic. We then created visualizations to present these trends to the initiative's stakeholders, to inform their decisions about how the university can help communities across the state. The results of our analysis can be viewed on the Carolina Across 100 website.",
+    projectspan: [],
+    projectimg: `./pictures/gif for CX100 page.gif`,
+    tab:['data-viz-tab','data-analysis-tab'],
+    link:'/top3.html'
+},
+{
+    projectid: 2,
+    projectname: 'RAPID: Examining the Innovative Ecosystem During the COVID-19 Pandemic',
+    projectkeywords: ['Policy Concentration', 'Policy Topic','Policy Context','Bar Chart', 'Yearly Evolution','Tableau'],
+    projctdescription: 'For this ongoing project, Dr. Johnson assembled a Relational Database Management System (RDMS) that brings together data from the USASpending, Systems for Award Management, and Dynamic Small Business Search databases. Data cleaning and analysis was also performed using Python. Dr. Johnson and his collaborators recently authored a paper about the RDMS which was published in a special issue of Research Policy..',
+    projectspan: [],
+    tab:['data-viz-tab','policy-evaluation-tab'],
+    projectimg: 'https://ars.els-cdn.com/content/image/1-s2.0-S0048733321001943-gr2.jpg',
+    link:'/top1.html'
+},
+{
+    projectid: 3,
+    projectname: 'Review of the SBIR and STTR programs at the Department of Energy',
+    projectkeywords: ['COVID-19', 'Survey Analysis', 'Data Visualization'],
+    projctdescription: "Dr. Johnson served as the Principal Consultant for the Review of the Small Business Innovation Research and Small Business Technology Transfer Programs at the Department of Energy." ,
+    projectspan: [],
+    tab:['policy-evaluation-tab'],
+    projectimg: 'https://www.nap.edu/cover/25674/450',
+    link:'/top2.html'
+
+}, ]
